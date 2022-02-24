aedc360037017bbccb10099c4d0d042cf809aec1
baby-kun <100061813+baby-kun@users.noreply.github.com>
2022-02-24 22:12:04 +0530

    Update __main__.py

--- Flare_Robot/__main__.py
+++ Flare_Robot/__main__.py
@@ -113,7 +113,7 @@ buttons = [
                        InlineKeyboardButton(text=" Help ", callback_data="help_back"),
                        InlineKeyboardButton(text="Chit Chat", url="https://t.me/OtaKu_Gang1"),
                        InlineKeyboardButton(text="Inline ", switch_inline_query_current_chat=""),
-                     
+                     ],
                     [                  
                        InlineKeyboardButton(
                              text="🚑 Support",
